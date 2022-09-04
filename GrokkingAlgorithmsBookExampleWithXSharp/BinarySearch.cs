967e992b49f5a6052eb3d7bfd82293c4980632f4
ali tamim <amoh331@gmail.com>
2022-09-05 02:55:59 +0300

    Add project files.

---
+++ GrokkingAlgorithmsBookExampleWithXSharp/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithmsBookExampleWithXSharp
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] arr, int item)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = arr[mid];
+                if (guess == item)
+                {
+                    return guess;
+                }
+                else if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return default;
+        }
+    }
+}
---
+++ GrokkingAlgorithmsBookExampleWithXSharp/QuickSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithmsBookExampleWithXSharp
+{
+    public static class QuickSort
+    {
+        public static List<int> Sort(List<int> items)
+        {
+            if (items.Count < 2)
+            {
+                return items;
+            }
+            else
+            {
+                int piovt = items[0];
+                List<int> smaller = new List<int>();
+                List<int> larger = new List<int>();
+                for (int i = 1; i < items.Count; i++)
+                {
+                    if (items[i] <= piovt)
+                    {
+                        smaller.Add(items[i]);
+                    }
+                    else
+                    {
+                        larger.Add(items[i]);
+                    }
+                }
+                smaller.Add(piovt);
+                smaller = Sort(smaller);
+                larger = Sort(larger);
+                return smaller.Concat(larger).ToList();
+            }
+        }
+    }
+}

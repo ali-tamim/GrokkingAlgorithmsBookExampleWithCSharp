204931b1338995a9e5e7f85eb746a43691f9248e
ali tamim <amoh331@gmail.com>
2022-09-09 22:17:36 +0300

    update

---
+++ GrokkingAlgorithmsBookExampleWithCSharp/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithmsBookExampleWithXSharp
+{
+    public class Graph
+    {
+        LinkedList<int>[] _linkedListArray;
+
+        public Graph(int v)
+        {
+            _linkedListArray = new LinkedList<int>[v];
+        }
+
+        public void AddEdge(int u, int v, bool blnBiDie = true)
+        {
+            if (_linkedListArray[u] == null)
+            {
+                _linkedListArray[u] = new LinkedList<int>();
+                _linkedListArray[u].AddFirst(v);
+            }
+        }
+    }
+}
